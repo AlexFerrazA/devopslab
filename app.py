cb6861161801216c864a6d401a0fbe1a89091e2c
AlexFerrazA <alexandre_arrichiello@hotmail.com>
2021-10-02 18:03:39 -0300

    Frase final.

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 
 @app.route("/")
 def pagina_inicial():
-    return "MBA IMPACTA FULL DEVOPS NA VEIA"
+    return "MBA IMPACTA FULL  STACK DEVOPS - ALEXANDRE FERRAZ"
 
 if __name__ == '__main__':
     app.run(debug=True)
--- test.py
+++ test.py
@@ -17,7 +17,7 @@ class Test(unittest.TestCase):
 
     def test_conteudo(self):                                                                                                                                                                             
         # verifica o retorno do conteudo da pagina                                                                                                                                                       
-        self.assertEqual(self.result.data.decode('utf-8'), "MBA IMPACTA FULL DEVOPS NA VEIA")                                                                                                                                
+        self.assertEqual(self.result.data.decode('utf-8'), "MBA IMPACTA FULL  STACK DEVOPS - ALEXANDRE FERRAZ")                                                                                                                                
 
 if __name__ == "__main__":                                                                                                                                                                               
     print ('INICIANDO OS TESTES')                                                                                                                                                                        
