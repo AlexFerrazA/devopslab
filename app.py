b5d2f2e33ced84c4295e7aecf9817c7fd3eddbc1
AlexFerrazA <alexandre_arrichiello@hotmail.com>
2021-10-02 18:48:40 -0300

    Nome

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 
 @app.route("/")
 def pagina_inicial():
-    return "MBA IMPACTA FULL STACK DEVOPS - ALEXANDRE"
+    return "MBA IMPACTA FULL STACK DEVOPS - ALEXANDRE FERRAZ ARRICHIELLO"
 
 if __name__ == '__main__':
     app.run(debug=True)
